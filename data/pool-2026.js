ec8f623cb0027cf9d353588c65b0d2e01baa0842
Webs <webster.r.james@gmail.com>
2026-04-14 10:36:13 -0400

    Sunset site with final 2026 results

---
+++ data/pool-2026.js
@@ -0,0 +1,81 @@
+const pool2026 = {
+  year: 2026,
+  final: true,
+  entries: [
+    {
+      finalRank: 1,
+      name: 'Kenner',
+      totalEarnings: 7654500,
+      players: {
+        groupA: { name: 'Rory McIlroy', earnings: 4200000 },
+        groupB1: { name: 'Cameron Young', earnings: 1008000 },
+        groupB2: { name: 'Matt Fitzpatrick', earnings: 294000 },
+        groupC1: { name: 'Justin Rose', earnings: 1008000 },
+        groupC2: { name: 'Robert MacIntyre', earnings: 0 },
+        groupD1: { name: 'Shane Lowry', earnings: 136500 },
+        groupD2: { name: 'Russell Henley', earnings: 1008000 },
+        groupE: { name: 'JJ Spaun', earnings: 0 }
+      }
+    },
+    {
+      finalRank: 2,
+      name: 'Know Nothing About Golf',
+      totalEarnings: 7551600,
+      players: {
+        groupA: { name: 'Rory McIlroy', earnings: 4200000 },
+        groupB1: { name: 'Ludvig Åberg', earnings: 235200 },
+        groupB2: { name: 'Xander Schauffele', earnings: 588000 },
+        groupC1: { name: 'Justin Rose', earnings: 1008000 },
+        groupC2: { name: 'Hideki Matsuyama', earnings: 399000 },
+        groupD1: { name: 'Russell Henley', earnings: 1008000 },
+        groupD2: { name: 'Min Woo Lee', earnings: 0 },
+        groupE: { name: 'Tiger Woods', earnings: 113400 }
+      }
+    },
+    {
+      finalRank: 3,
+      name: 'Ana G.',
+      totalEarnings: 6552000,
+      players: {
+        groupA: { name: 'Rory McIlroy', earnings: 4200000 },
+        groupB1: { name: 'Tommy Fleetwood', earnings: 189000 },
+        groupB2: { name: 'Xander Schauffele', earnings: 588000 },
+        groupC1: { name: 'Brooks Koepka', earnings: 399000 },
+        groupC2: { name: 'Patrick Reed', earnings: 399000 },
+        groupD1: { name: 'Russell Henley', earnings: 1008000 },
+        groupD2: { name: 'Joaquin Niemann', earnings: 0 },
+        groupE: { name: 'Tiger Woods', earnings: 113400 }
+      }
+    },
+    {
+      finalRank: 4,
+      name: 'I Like Big Putts and I Cannot Lie!',
+      totalEarnings: 6438600,
+      players: {
+        groupA: { name: 'Scottie Scheffler', earnings: 2268000 },
+        groupB1: { name: 'Cameron Young', earnings: 1008000 },
+        groupB2: { name: 'Ludvig Åberg', earnings: 235200 },
+        groupC1: { name: 'Justin Rose', earnings: 1008000 },
+        groupC2: { name: 'Hideki Matsuyama', earnings: 399000 },
+        groupD1: { name: 'Sam Burns', earnings: 677250 },
+        groupD2: { name: 'Corey Conners', earnings: 0 },
+        groupE: { name: 'Bernhard Langer', earnings: 113400 }
+      }
+    },
+    {
+      finalRank: 5,
+      name: 'Shea DAWG',
+      totalEarnings: 6417600,
+      players: {
+        groupA: { name: 'Scottie Scheffler', earnings: 2268000 },
+        groupB1: { name: 'Matt Fitzpatrick', earnings: 294000 },
+        groupB2: { name: 'Xander Schauffele', earnings: 588000 },
+        groupC1: { name: 'Justin Rose', earnings: 1008000 },
+        groupC2: { name: 'Hideki Matsuyama', earnings: 399000 },
+        groupD1: { name: 'Sam Burns', earnings: 677250 },
+        groupD2: { name: 'Corey Conners', earnings: 0 },
+        groupE: { name: 'Tiger Woods', earnings: 113400 }
+      }
+    }
+  ]
+};
